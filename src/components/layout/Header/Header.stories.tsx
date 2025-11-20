f5c7a7a0d30dd6b111aff81e731cd10b19e56961
chahyunlee <chahyun023@gmail.com>
2025-11-20 22:21:04 +0900

    feat: 섹션별 variant 설정(#1)

--- src/components/layout/Header/Header.stories.tsx
+++ src/components/layout/Header/Header.stories.tsx
@@ -7,14 +7,39 @@ const meta = {
   parameters: {
     layout: "fullscreen",
   },
-  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["dark", "light", "gray"],
+      description: "섹션에 따른 헤더 색상 변경",
+    },
+  },
 } satisfies Meta<typeof Header>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const Dark: Story = {
+  args: {
+    variant: "dark",
+  },
+};
+
+export const Light: Story = {
+  args: {
+    variant: "light",
+  },
+};
+
+export const Gray: Story = {
+  args: {
+    variant: "gray",
+  },
+};
+
+//디폴트
 export const Default: Story = {
   args: {
-    logoAlt: "Makers",
+    variant: "dark",
   },
 };
